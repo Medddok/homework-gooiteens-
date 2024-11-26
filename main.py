4d6ac46c448b8521e1f93be16cdda741d918fcf7
Medddok <tsarik.junior@gmail.com>
2024-11-26 19:40:01 +0100

    homework 1 (26.11.2024)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#test
+# homework 1 26.11.2024
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/calculate")
+async def calculate(operation: str, num1: float, num2: float):
+    if operation == "+":
+        return {"result": num1 + num2}
+    elif operation == "-":
+        return {"result": num1 - num2}
+    return {"error": "Invalid operation. Use '+' or '-'."}
+
+if __name__ == "__main__":
+    print("Server is running at: http://127.0.0.1:8000/calculate")
+    uvicorn.run(app, host="127.0.0.1", port=8000)
