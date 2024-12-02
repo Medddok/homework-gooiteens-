d99a9b637e82d8635fd91f7eef4edf6a2d55bf02
Medddok <tsarik.junior@gmail.com>
2024-12-02 20:05:40 +0100

    2.12.2024*

--- main3.py
+++ main3.py
@@ -4,12 +4,10 @@ from typing import List
 
 app = FastAPI()
 
-# Список для зберігання імен
 names_list = []
 
 @app.post("/add_name/")
 def add_name(name: str):
-    """Додає нове ім'я до списку."""
     if name in names_list:
         raise HTTPException(status_code=400, detail="Name already exists.")
     names_list.append(name)
@@ -17,12 +15,10 @@ def add_name(name: str):
 
 @app.get("/get_names/", response_model=List[str])
 def get_names():
-    """Повертає список всіх збережених імен."""
     return names_list
 
 @app.delete("/delete_name/")
 def delete_name(name: str):
-    """Видаляє ім'я зі списку."""
     if name not in names_list:
         raise HTTPException(status_code=404, detail="Name not found.")
     names_list.remove(name)
